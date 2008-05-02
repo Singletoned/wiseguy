f56be53733f9ee4c24088deb47afa7dcf87eed84
Ed Singleton <singletoned@gmail.com>
2008-05-02 14:39:04 +0100

    Moved simple_decorator to picard/utils

--- wiki/utils.py
+++ wiki/utils.py
@@ -9,6 +9,8 @@ from werkzeug.routing import Map, Rule
 
 from templating import render_template
 
+from picard.utils import simple_decorator
+
 local = Local()
 local_manager = LocalManager([local])
 application = local('application')
@@ -18,29 +20,6 @@ session = scoped_session(lambda: create_session(application.database_engine,
                          transactional=True), local_manager.get_ident)
 
 url_map = Map()
-
-def simple_decorator(decorator):
-    """This decorator can be used to turn simple functions
-    into well-behaved decorators, so long as the decorators
-    are fairly simple. If a decorator expects a function and
-    returns a function (no descriptors), and if it doesn't
-    modify function attributes or docstring, then it is
-    eligible to use this. Simply apply @simple_decorator to
-    your decorator and it will automatically preserve the
-    docstring and function attributes of functions to which
-    it is applied."""
-    def new_decorator(f):
-        g = decorator(f)
-        g.__name__ = f.__name__
-        g.__doc__ = f.__doc__
-        g.__dict__.update(f.__dict__)
-        return g
-    # Now a few lines needed to make simple_decorator itself
-    # be a well-behaved decorator.
-    new_decorator.__name__ = decorator.__name__
-    new_decorator.__doc__ = decorator.__doc__
-    new_decorator.__dict__.update(decorator.__dict__)
-    return new_decorator
 
 def expose(rule, methods=['GET'], **kw):
     @simple_decorator
