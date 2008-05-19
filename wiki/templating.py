37e0ee483590cbc3da00681b2991c0d28d4a25b1
Ed Singleton <singletoned@gmail.com>
2008-05-19 16:09:34 +0100

    Deleted templating.py (it is empty and pointless)

--- wiki/templating.py
+++
@@ -1,7 +0,0 @@
-from jinja import Environment, FileSystemLoader
-
-env = Environment(loader=FileSystemLoader('templates'))
-
-def render_template(template_name, *args, **kwargs):
-	"""Gets a template and renders it"""
-	return env.get_template('%s.html' % template_name).render(kwargs)
