5a834fd533f363eb43601a619b29b95cedc23d45
Ed Singleton <singletoned@gmail.com>
2008-05-19 15:56:42 +0100

    Moved render_class, and expose_class to picard/utils.py

--- picard/utils.py
+++ picard/utils.py
@@ -32,4 +32,22 @@ def create_expose(url_map):
             return f
         return decorate
     return expose
-
+# 
+# def expose_class(rule, methods=['GET'], **kw):
+#     def decorate(f):
+#         kw['endpoint'] = f.__name__
+#         url_map.add(Rule(rule, methods=methods, **kw))
+#         return f
+#     decorate_class(decorate)
+# 
+# def render_class(template_name, mimetype='text/html'):
+#     def decorate(f):
+#         def func(request, *args, **kwargs):
+#             values = getattr(f, request.method.upper())(request, *args, **kwargs)
+#             try:
+#                 return Response(render_template(template_name, **values), mimetype=mimetype)
+#             except TypeError:
+#                 return values
+#         return func
+#     decorate_class(decorate)
+# 
