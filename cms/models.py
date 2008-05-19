42ebc5d9ec8f5a137cc323c0cdfc360012c29622
Ed Singleton <singletoned@gmail.com>
2008-05-19 22:57:22 +0100

    Added a fake ContentList to hold page contents

--- cms/models.py
+++ cms/models.py
@@ -6,6 +6,25 @@ from couchdb.client import Server, ResourceNotFound
 
 from picard.schema import PicardDocument, TextField, IntegerField, DateTimeField, DictField, ListField, FloatField, Schema
 
+class ContentsList(object):
+    """A fake list for page content items"""
+    def __init__(self, parent, keys):
+        self._keys = keys
+        self._parent = parent
+    
+    def __getitem__(self, i):
+        key = self._keys[i]
+        return Content.by_id(key)
+    
+    def __len__(self):
+        return len(self._keys)
+    
+    def append(self, item):
+        self._parent.content_keys.append(item.id)
+        self._parent.save()
+    
+    def __repr__(self):
+        return str([Content.by_id(key) for key in self._keys])
 
 
 class Page(PicardDocument):
@@ -14,9 +33,14 @@ class Page(PicardDocument):
     
     address = TextField(keyable=True)
     title = TextField(keyable=True)
-    contents = ListField(
-        TextField()
+    content_keys = ListField(
+        TextField(),
+        default=[]
     )
+    
+    @property
+    def contents(self):
+        return ContentsList(self, self.content_keys)
 
 
 class Content(PicardDocument):
@@ -35,7 +59,7 @@ class User(PicardDocument):
     username = TextField(keyable=True)
     email = TextField(keyable=True)
     password = TextField()
-
+    
     @classmethod
     def create_from_form(cls, form):
         """Ignores extra fields"""
