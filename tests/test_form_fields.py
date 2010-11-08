e960945126a9603d99a688c261dc01e2ba93c767
Ed Singleton <singletoned@gmail.com>
2010-11-08 19:02:54 +0000

    Added password and submit form_fields

--- tests/test_form_fields.py
+++ tests/test_form_fields.py
@@ -50,3 +50,67 @@ class TestInput(unittest.TestCase):
         '''.strip()
         result = form_fields.input(context, 'foo', "Foo:")
         assert expected == result
+
+
+class TestPassword(unittest.TestCase):
+    def test_plain(self):
+        context = dict(data=None, errors=None)
+        expected = '''
+<label for="foo">Foo:</label>
+<input type="password" name="foo" value="" id="foo">
+        '''.strip()
+        result = form_fields.password(context, 'foo', "Foo:")
+        assert expected == result
+
+    def test_empty(self):
+        context = dict()
+        expected = '''
+<label for="foo">Foo:</label>
+<input type="password" name="foo" value="" id="foo">
+        '''.strip()
+        result = form_fields.password(context, 'foo', "Foo:")
+        assert expected == result
+
+    def test_compulsory(self):
+        context = dict()
+        expected = '''
+<label for="foo">Foo:*</label>
+<input type="password" name="foo" value="" id="foo">
+        '''.strip()
+        result = form_fields.password(context, 'foo', "Foo:", compulsory=True)
+        assert expected == result
+
+    def test_data(self):
+        context = dict(data=dict(foo='blah'), errors=None)
+        expected = '''
+<label for="foo">Foo:</label>
+<input type="password" name="foo" value="" id="foo">
+        '''.strip()
+        result = form_fields.password(context, 'foo', "Foo:")
+        assert expected == result
+
+    def test_errors(self):
+        context = dict(data=dict(), errors=dict(foo="Please enter a foo"))
+        expected = '''
+<label for="foo">Foo:</label>
+<input type="password" name="foo" value="" id="foo">
+<span class="error">Please enter a foo</span>
+        '''.strip()
+        result = form_fields.password(context, 'foo', "Foo:")
+        assert expected == result
+
+class TestSubmit(unittest.TestCase):
+    def test_plain(self):
+        expected = '''<input type="submit" id="submit" value="Submit">'''
+        result = form_fields.submit()
+        assert expected == result
+
+    def test_with_labels(self):
+        expected = '''<input type="submit" id="foo" value="Foo!">'''
+        result = form_fields.submit('foo', "Foo!")
+        assert expected == result
+
+    def test_bad_value(self):
+        expected = '''<input type="submit" id="foo" value="Foo&amp;&lt;">'''
+        result = form_fields.submit('foo', "Foo&<")
+        assert expected == result
--- wiseguy/form_fields.py
+++ wiseguy/form_fields.py
@@ -27,3 +27,31 @@ def input(context, id, label, compulsory=False):
     return '\n'.join(elements)
 
 
+@j2.contextfunction
+def password(context, id, label, compulsory=False):
+    if compulsory:
+        label = label + "*"
+    elements = [
+        html.label(
+            label,
+            for_=id),
+        html.input(
+            type="password",
+            name=id,
+            id=id,
+            value="")]
+    if context.get('errors', None):
+        if context['errors'].get(id, ''):
+            elements.append(
+                html.span(
+                    context['errors'][id],
+                    class_='error'))
+    return '\n'.join(elements)
+
+
+def submit(id="submit", label="Submit"):
+    element = html.input(
+        type="submit",
+        id=id,
+        value=label)
+    return element
