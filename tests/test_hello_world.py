19df37e043f265eb867d24cdb13aa511104df45a
Ed Singleton <singletoned@gmail.com>
2011-11-03 09:49:38 +0000

    Added a basic test for a Hello World application

---
+++ tests/test_hello_world.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import wiseguy.web_utils
+import werkzeug as wz
+import jinja2
+
+url_map = wz.routing.Map()
+expose = wiseguy.web_utils.create_expose(url_map)
+
+@expose(u"/")
+@wiseguy.web_utils.render('index.html')
+def index(request):
+    return dict(greeting="Hello", name="World")
+
+env = jinja2.Environment(
+    loader=jinja2.DictLoader(
+        {'index.html': "<p>{{greeting}}, {{name}}!</p>"}))
+
+application = wiseguy.web_utils.BaseApp(
+    config=dict(),
+    url_map=url_map,
+    env=env)
+
+def test_hello_world():
+    import testino
+    agent = testino.TestAgent(application)
+    page = agent.get("/")
+    assert page.one("//p[text()=$text]", text="Hello, World!")
