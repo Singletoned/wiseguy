76e501c1addb19512cc79d5c116b91d42e1ac502
Ed Singleton <singletoned@gmail.com>
2012-02-01 22:11:32 +0000

    Added a span wrapper

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import unittest
+
+import lxml.html
 from lxml.html import builder as html
 
-from wiseguy import wrappers
+from wiseguy import form_fields, wrappers
 
 def test_add_class():
     d = html.DIV()
     wrappers.add_class(d, 'foo')
     assert d.attrib['class'] == 'foo'
+
+
+
+class TestWrappers(unittest.TestCase):
+    bootstrap_form_fields = form_fields.BootstrapFormFields()
+
+    def test_span5(self):
+        context = dict(data=None, errors=None)
+        expected = '''
+<fieldset class="control-group">
+<label for="foo" class="control-label">Foo:</label><div class="controls"><input type="text" id="foo" value="" name="foo" class="span5"></div>
+</fieldset>'''.strip()
+        result = wrappers.span(
+            5,
+            self.bootstrap_form_fields.input(context, 'foo', "Foo:"))
+        result = lxml.html.tostring(result, pretty_print=True)
+        result = result.strip()
+        assert expected == result
--- wiseguy/wrappers.py
+++ wiseguy/wrappers.py
@@ -4,3 +4,8 @@ def add_class(element, class_name):
     classes = element.attrib.get('class', "").split()
     classes.append(class_name)
     element.attrib['class'] = " ".join(classes)
+
+def span(length, element):
+    _input = element.xpath('//input')[0]
+    add_class(_input, 'span%s'%length)
+    return element
