89f6875d8df60d73e62d954fbadb3e493e1e6bb2
Ed Singleton <singletoned@gmail.com>
2012-02-02 15:17:27 +0000

    Renamed add_class to _add_class

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -10,7 +10,7 @@ from wiseguy import form_fields, wrappers
 
 def test_add_class():
     d = html.DIV()
-    wrappers.add_class(d, 'foo')
+    wrappers._add_class(d, 'foo')
     assert d.attrib['class'] == 'foo'
 
 
--- wiseguy/wrappers.py
+++ wiseguy/wrappers.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def add_class(element, class_name):
+def _add_class(element, class_name):
     classes = element.attrib.get('class', "").split()
     classes.append(class_name)
     element.attrib['class'] = " ".join(classes)
 
 def span(length, element):
     _input = element.xpath('//input')[0]
-    add_class(_input, 'span%s'%length)
+    _add_class(_input, 'span%s'%length)
     return element
 
 def compulsory(element):
