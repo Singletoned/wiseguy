c1557ba1437f88c2b5bc791c75c3bf1189e7733b
Ed Singleton <singletoned@gmail.com>
2012-10-01 13:19:59 +0100

    Added html_tags module

---
+++ wiseguy/html_tags.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import wiseguy.html
+
+builder = wiseguy.html.HtmlBuilder()
+
+
+HTML = builder.html
+HEAD = builder.head
+LINK = builder.link
+META = builder.meta
+STYLE = builder.style
+BODY = builder.body
+TABLE = builder.table
+TR = builder.tr
+TD = builder.td
+IMG = builder.img
+SPAN = builder.span
+DIV = builder.div
+BR = builder.br
+P = builder.p
+A = builder.a
