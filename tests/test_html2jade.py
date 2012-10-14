21e9454207794578caf563a7e7562b2566a7b96c
Ed Singleton <singletoned@gmail.com>
2012-10-14 15:21:46 +0100

    Used files for html2jade test cases

--- tests/test_html2jade.py
+++ tests/test_html2jade.py
@@ -2,20 +2,17 @@
 
 from wiseguy import html2jade
 
+import path
 
-data = (
-    (
-        '''<p>Hello</p>''',
-        '''p Hello'''),
-    (
-        '''<div><p>Hello</p><p>World</p></div>''',
-        '''
-div
-  p Hello
-  p World'''.strip()),
-)
+
+test_dir = path.path(__file__).dirname()
+html2jade_dir = test_dir.child("test_html2jade_cases")
+
+case_names = set(f.namebase for f in html2jade_dir.files())
 
 def test_simple():
-    for (html, expected) in data:
+    for case_name in case_names:
+        html = html2jade_dir.child("%s.html"%case_name).text()
+        expected = html2jade_dir.child("%s.jade"%case_name).text().strip()
         result = html2jade.html2jade(html)
         assert result == expected
