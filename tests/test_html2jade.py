f0fe4a32718765dec6cccde9c7255561891d6417
Ed Singleton <singletoned@gmail.com>
2012-10-14 16:22:36 +0100

    Slightly refactored html2jade tests

--- tests/test_html2jade.py
+++ tests/test_html2jade.py
@@ -11,8 +11,11 @@ html2jade_dir = test_dir.child("test_html2jade_cases")
 case_names = set(f.namebase for f in html2jade_dir.files())
 
 def test_simple():
-    for case_name in case_names:
+    def do_test(case_name):
         html = html2jade_dir.child("%s.html"%case_name).text()
         expected = html2jade_dir.child("%s.jade"%case_name).text().strip()
         result = html2jade.html2jade(html)
         assert result == expected
+
+    for case_name in case_names:
+        yield do_test, case_name
