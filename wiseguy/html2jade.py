b163c40f42fdded28ddf9e6c9bed730713b4ba95
Ed Singleton <singletoned@gmail.com>
2012-10-14 16:22:56 +0100

    Added classes to html2jade

--- wiseguy/html2jade.py
+++ wiseguy/html2jade.py
@@ -6,6 +6,8 @@ import wiseguy.html
 def render_el(el):
     parts = []
     parts.append(el.tag)
+    if el.attrib.has_key('class'):
+        parts.append("."+el.attrib['class'].strip())
     if el.text and el.text.strip():
         parts.append(" "+el.text)
     yield "".join(parts)
