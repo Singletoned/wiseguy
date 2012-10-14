07652a552b52e0a0df9834ddfa7bc12ffbe09deb
Ed Singleton <singletoned@gmail.com>
2012-10-14 16:34:36 +0100

    Fixed multiple classes in html2jade

--- wiseguy/html2jade.py
+++ wiseguy/html2jade.py
@@ -9,7 +9,8 @@ def render_el(el):
     if el.attrib.has_key('id'):
         parts.append("#"+el.attrib['id'].strip())
     if el.attrib.has_key('class'):
-        parts.append("."+el.attrib['class'].strip())
+        for cls in el.attrib['class'].split():
+            parts.append("."+cls.strip())
     if el.text and el.text.strip():
         parts.append(" "+el.text)
     yield "".join(parts)
