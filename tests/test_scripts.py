abd845313aa2341f648747330387a95f71f1ea19
Ed Singleton <singletoned@gmail.com>
2012-10-15 16:04:12 +0100

    Refactored scripts.parse_jade

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -9,14 +9,17 @@ import wiseguy.scripts.parse_jade
 
 
 def test_parse_jade():
-    data = "html: body: div#main"
-    expected = '''
+    jade_input = "html: body: div#main"
+    html_expected = '''
 <html>
   <body>
     <div id="main"></div>
   </body>
 </html>'''.strip()
-    with path.create_temp_file(data) as filepath:
-        with path.temp_sys_argv("python", filepath):
-            result = wiseguy.scripts.parse_jade.main()
-    assert expected == result
+    with path.create_temp_dir() as temp_dir:
+        temp_html = temp_dir.child("test.html")
+        temp_jade = temp_dir.child("test.jade")
+        temp_jade.write_text(jade_input)
+        with path.temp_sys_argv("python", temp_jade):
+            wiseguy.scripts.parse_jade.main()
+        assert temp_html.text() == html_expected
--- wiseguy/scripts/parse_jade.py
+++ wiseguy/scripts/parse_jade.py
@@ -2,13 +2,20 @@
 
 import sys
 
+import path
+
 import wiseguy.html_tidy
+import wiseguy.html
+
 
 def main():
-    arg = sys.argv[1]
-    with open(arg) as f:
-        content = f.read()
-        return wiseguy.html_tidy.tidy_html(wiseguy.html.jade(content))
+    args = sys.argv[1:]
+    for arg in args:
+        f_in = path.path(arg)
+        f_out = f_in.parent.child(f_in.namebase + ".html")
+        jade_content = f_in.text('utf8')
+        html_content = wiseguy.html_tidy.tidy_html(wiseguy.html.jade(jade_content))
+        f_out.write_text(html_content, 'utf8')
 
 if __name__ == '__main__':
-    print main()
+    main()
