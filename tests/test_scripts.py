aab7558d521781c4e3eb51442ea54c7c840f1be5
Ed Singleton <singletoned@gmail.com>
2012-10-15 16:04:32 +0100

    Added html2jade script

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -5,7 +5,7 @@ import sys
 import path
 
 import wiseguy.scripts.parse_jade
-
+import wiseguy.scripts.html2jade
 
 
 def test_parse_jade():
@@ -23,3 +23,27 @@ def test_parse_jade():
         with path.temp_sys_argv("python", temp_jade):
             wiseguy.scripts.parse_jade.main()
         assert temp_html.text() == html_expected
+
+def test_html2jade():
+    html_input = '''
+<html>
+  <head></head>
+  <body>
+    <div>
+      <p>This cost £££</p>
+    </div>
+  </body>
+</html>'''.strip()
+    jade_expected = '''
+html
+  head
+  body
+    div
+      p This cost £££'''.strip()
+    with path.create_temp_dir() as temp_dir:
+        temp_html = temp_dir.child("test.html")
+        temp_jade = temp_dir.child("test.jade")
+        temp_html.write_text(html_input)
+        with path.temp_sys_argv("python", temp_html):
+            wiseguy.scripts.html2jade.main()
+        assert temp_jade.text() == jade_expected
