324a9e31222d589d0e29fec1d6de6803e504a14d
Ed Singleton <singletoned@gmail.com>
2012-10-18 12:15:38 +0100

    Added a fix_urls transform

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import werkzeug
+
 import wiseguy.transforms
 import wiseguy.html
 
@@ -30,4 +32,37 @@ def test_add_script():
     expected = """
 <html><head><script src="url:foo.js"></script></head></html>
     """.strip()
+    assert result == expected
+
+
+def test_fix_urls():
+    transform = wiseguy.transforms.fix_urls()
+    element = wiseguy.html.jade('''
+html
+  head
+    link(href="/static/blueprint.css", type="text/css")
+    script(src="/static/jquery.css")
+  body
+    p
+      a(href="/a_link.html")
+    form(method="POST", action="form_handler")
+''')
+
+    assert transform.keys == set(['url'])
+    transform.action(
+        element=element,
+        url=werkzeug.Href("/mountpoint"))
+    expected = '''
+<html>
+<head>
+<link href="/mountpoint/static/blueprint.css" type="text/css">
+<script src="/mountpoint/static/jquery.css"></script>
+</head>
+<body>
+<p><a href="/mountpoint/a_link.html"></a></p>
+<form method="POST" action="form_handler"></form>
+</body>
+</html>
+'''.strip()
+    result = element.to_string().strip()
     assert result == expected
--- wiseguy/transforms.py
+++ wiseguy/transforms.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import cssselect
+css = cssselect.HTMLTranslator().css_to_xpath
+
 from wiseguy import html_tags as ht
 from wiseguy.template import Transform
 
@@ -29,3 +32,22 @@ def add_script(href):
             "head",
             script(
                 url(href))))
+
+_url_fixable_tags = set([
+    ("link", "href"),
+    ("script", "src"),
+    ("a", "href"),
+    ("form", "action")
+])
+
+def _fix_urls(element, url):
+    for (tag, attr) in _url_fixable_tags:
+        path = css("%s[%s]" % (tag, attr))
+        for el in element.xpath(path):
+            if el.attrib[attr].startswith("/"):
+                el.attrib[attr] = url(el.attrib[attr])
+
+def fix_urls():
+    return Transform(
+        "url",
+        lambda element, url: _fix_urls(element, url))
